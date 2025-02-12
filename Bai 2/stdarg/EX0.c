42045f774a9e8798df3eaa0174bddcccbd16b69c
NguyenDuc <minhnguyenduc1205@gmail.com>
2025-02-12 23:12:03 +0700

    Intro to stdarg lib

---
+++ Bai 2/stdarg/EX0.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdarg.h>
+
+void test(int count,...){ // count: so luong doi so minh truyen vao
+
+    va_list args;
+    // typedef char* va_list
+    // char *args ="int count, 1, 2, 6"
+    
+    va_start(args, count);
+
+    // va_arg(args, int); Dung de truy cap vao cac doi so
+
+    printf("value 1: %d\n",va_arg(args, int));
+    printf("value 2: %d\n",va_arg(args, int));
+    printf("value 3: %d\n",va_arg(args, int));
+    printf("value 4: %f\n",va_arg(args, double));
+    printf("%s\n",va_arg(args, char*));
+
+    va_end(args);
+
+}
+
+int main(){
+    test(5,1,2,6,3.19,"hello"); // count == 5
+    return 0;
+}
+
+ 
